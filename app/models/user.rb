1e538a04a225de3bcb2be9b153b5312cbd830cee
Luke Niedermyer <luke@westarete.com>
2018-02-09 16:18:22 -0500

    Add Devise functionality to User
    bin/rails generate devise User
    
    - Add correct modules to User model
    - Update migration accordingly
    - Add component specs for new columns/validations
    - update devise initializer with correct options
    - run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  devise :database_authenticatable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable,
+         :lockable
+
   validates :first_name,
             :last_name,
             presence: true
--- spec/component/models/user_spec.rb
+++ spec/component/models/user_spec.rb
@@ -9,11 +9,35 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:administrator).of_type(:boolean).with_options(null: false, default: false) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+
+    it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false, default: "") }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(null: false, default: "") }
+    it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
+    it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:sign_in_count).of_type(:integer).with_options(null: false, default: 0) }
+    it { is_expected.to have_db_column(:current_sign_in_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:last_sign_in_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:current_sign_in_ip).of_type(:inet) }
+    it { is_expected.to have_db_column(:last_sign_in_ip).of_type(:inet) }
+    it { is_expected.to have_db_column(:confirmation_token).of_type(:string) }
+    it { is_expected.to have_db_column(:confirmed_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:confirmation_sent_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:unconfirmed_email).of_type(:string) }
+    it { is_expected.to have_db_column(:failed_attempts).of_type(:integer).with_options(null: false, default: 0) }
+    it { is_expected.to have_db_column(:locked_at).of_type(:datetime) }
+
+    it { is_expected.to have_db_index(:email).unique(true) }
+    it { is_expected.to have_db_index(:reset_password_token).unique(true) }
+    it { is_expected.to have_db_index(:confirmation_token).unique(true) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8).is_at_most(128) }
+    it { is_expected.to allow_value("email@example.com").for(:email) }
+    it { is_expected.not_to allow_value("invalidemail").for(:email) }
   end
 
 end
