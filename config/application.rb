2a533a2f346e07ff99b7e0cb6fc4605a4508db00
Luke Niedermyer <luke@westarete.com>
2018-02-09 14:09:06 -0500

    Load our configuration files immediately

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module ActivityLog
   class Application < Rails::Application
+
+    # Load all of our configuration files into Rails.configuration.x.* objects.
+    # These will be available throughout other initializers, so that we don't
+    # have to manually read config files in several places.
+    %i{ smtp }.each do |name|
+      if File.readable?(Rails.root.join("config/#{name}.yml"))
+        config.x.send("#{name}=", OpenStruct.new(Rails.application.config_for(name)))
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
