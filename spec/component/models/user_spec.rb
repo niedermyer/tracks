b5ab0ed43771c853d0a46b03c17b5664e280a9e1
Luke Niedermyer <luke@westarete.com>
2018-02-09 12:15:29 -0500

    Create Users

---
+++ spec/component/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'component/component_spec_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'the users table' do
+    subject { User.new }
+    it { is_expected.to have_db_column(:first_name).of_type(:string) }
+    it { is_expected.to have_db_column(:last_name).of_type(:string) }
+    it { is_expected.to have_db_column(:administrator).of_type(:boolean).with_options(null: false, default: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+  end
+
+end
