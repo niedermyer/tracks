554c9cc8ec91d050866873b718e3b20ecc787459
Luke Niedermyer <luke@westarete.com>
2018-02-11 18:53:37 -0500

    Define root for authenticated and unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,20 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    authenticated :user do
+      root 'user/dashboard#show', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   namespace :user do
     get '/dashboard', to: 'dashboard#show'
   end
 
-  root to: 'public#home'
+  root to: 'user/dashboard#show'
 
 end
